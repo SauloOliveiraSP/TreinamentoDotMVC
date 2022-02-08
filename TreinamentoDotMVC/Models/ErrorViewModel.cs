21e89da34c329c6649b1be8728d785f508010dd2
SauloOliveiraSP <sauloliveira.sp@gmail.com>
2022-02-08 09:22:57 -0300

    Adicionar arquivos de projeto.

---
+++ TreinamentoDotMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TreinamentoDotMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
